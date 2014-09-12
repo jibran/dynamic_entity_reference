cb2e89cea9680d7dee51aa3a284e9b0171a9131b
berdir <berdir@214652.no-reply.drupal.org>
2014-09-12 16:02:57 +1000

    Issue #2332117 by Berdir: Added a DynamicEntityReferenceItemNormalizer.

---
+++ src/Normalizer/DynamicEntityReferenceItemNormalizer.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\dynamic_entity_reference\Normalizer\EntityReferenceItemNormalizer.
+ */
+
+namespace Drupal\dynamic_entity_reference\Normalizer;
+
+use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\hal\Normalizer\EntityReferenceItemNormalizer;
+use Drupal\rest\LinkManager\LinkManagerInterface;
+use Drupal\serialization\EntityResolver\EntityResolverInterface;
+
+/**
+ * Dynamic entity reference normalizer.
+ */
+class DynamicEntityReferenceItemNormalizer extends EntityReferenceItemNormalizer {
+
+  /**
+   * The interface or class that this Normalizer supports.
+   *
+   * @var string
+   */
+  protected $supportedInterfaceOrClass = 'Drupal\dynamic_entity_reference\Plugin\Field\FieldType\DynamicEntityReferenceItem';
+
+  /**
+   * {@inheritdoc}
+   */
+  public function normalize($field_item, $format = NULL, array $context = array()) {
+    $normalized = parent::normalize($field_item, $format, $context);
+    $normalized['target_type'] = $field_item->target_type;
+    return $normalized;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function constructValue($data, $context) {
+    if ($id = $this->entityResolver->resolve($this, $data, $data['target_type'])) {
+      return array(
+        'target_type' => $data['target_type'],
+        'target_id' => $id,
+      );
+    }
+    return NULL;
+  }
+
+}
