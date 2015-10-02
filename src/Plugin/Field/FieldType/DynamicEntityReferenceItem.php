123d4bd6ae9b1299ccb07f0cb9912c8432e4a5df
Jibran Ijaz <jibran@1198144.no-reply.drupal.org>
2015-10-02 23:11:54 +0500

    Issue #2576533 by jibran: Rename SafeStringInterface to MarkupInterface and move related classes

--- src/Plugin/Field/FieldType/DynamicEntityReferenceItem.php
+++ src/Plugin/Field/FieldType/DynamicEntityReferenceItem.php
@@ -14,7 +14,7 @@ use Drupal\Core\Field\FieldItemBase;
 use Drupal\Core\Field\FieldStorageDefinitionInterface;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Session\AccountInterface;
-use Drupal\Core\StringTranslation\TranslationWrapper;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
 use Drupal\Core\TypedData\DataDefinition;
 use Drupal\dynamic_entity_reference\DataDynamicReferenceDefinition;
 use Drupal\entity_reference\ConfigurableEntityReferenceItem;
@@ -83,12 +83,12 @@ class DynamicEntityReferenceItem extends ConfigurableEntityReferenceItem {
       ->setRequired(TRUE);
 
     $properties['target_type'] = DataDefinition::create('string')
-      ->setLabel(new TranslationWrapper('Target Entity Type'))
+      ->setLabel(new TranslatableMarkup('Target Entity Type'))
       ->setRequired(TRUE);
 
     $properties['entity'] = DataDynamicReferenceDefinition::create('entity')
       ->setLabel(t('Entity'))
-      ->setDescription(new TranslationWrapper('The referenced entity'))
+      ->setDescription(new TranslatableMarkup('The referenced entity'))
       // The entity object is computed out of the entity ID.
       ->setComputed(TRUE)
       ->setReadOnly(FALSE);
