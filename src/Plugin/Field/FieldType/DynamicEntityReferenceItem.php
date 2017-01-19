8a1da4e1cee568435513bb7d12303c26257ebd3f
jibran <jibran@1198144.no-reply.drupal.org>
2017-01-20 00:51:14 +1100

    Issue #2805659 by jibran, marcvangend, larowlan: Change the DER field type group in the field_ui

--- src/Plugin/Field/FieldType/DynamicEntityReferenceItem.php
+++ src/Plugin/Field/FieldType/DynamicEntityReferenceItem.php
@@ -31,7 +31,7 @@ use Drupal\dynamic_entity_reference\DataDynamicReferenceDefinition;
  *   id = "dynamic_entity_reference",
  *   label = @Translation("Dynamic entity reference"),
  *   description = @Translation("An entity field containing a dynamic entity reference."),
- *   category = @Translation("Reference"),
+ *   category = @Translation("Dynamic Reference"),
  *   no_ui = FALSE,
  *   list_class = "\Drupal\dynamic_entity_reference\Plugin\Field\FieldType\DynamicEntityReferenceFieldItemList",
  *   default_widget = "dynamic_entity_reference_default",
@@ -599,6 +599,35 @@ class DynamicEntityReferenceItem extends EntityReferenceItem {
       }
     }
     return $dependencies;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getPreconfiguredOptions() {
+    $options = [];
+
+    // Add all the commonly referenced entity types as distinct pre-configured
+    // options.
+    $entity_types = \Drupal::entityTypeManager()->getDefinitions();
+    $common_references = array_filter($entity_types, function (EntityTypeInterface $entity_type) {
+      return $entity_type->isCommonReferenceTarget();
+    });
+
+    /** @var \Drupal\Core\Entity\EntityTypeInterface $entity_type */
+    foreach ($common_references as $entity_type) {
+      $options[$entity_type->id()] = [
+        'label' => $entity_type->getLabel(),
+        'field_storage_config' => [
+          'settings' => [
+            'exclude_entity_types' => FALSE,
+            'entity_type_ids' => [$entity_type->id()],
+          ],
+        ],
+      ];
+    }
+
+    return $options;
   }
 
 }
