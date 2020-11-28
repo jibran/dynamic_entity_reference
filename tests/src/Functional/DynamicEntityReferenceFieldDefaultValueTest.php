f562413872e4867455b7f5397fe9f40e70cf87b4
Jibran Ijaz <jibran.ijaz@gmail.com>
2020-11-28 22:26:10 +1100

    Issue #3168375 by jibran: Convert calls to drupalPostForm(NULL, ...) to submitForm

--- tests/src/Functional/DynamicEntityReferenceFieldDefaultValueTest.php
+++ tests/src/Functional/DynamicEntityReferenceFieldDefaultValueTest.php
@@ -183,7 +183,8 @@ class DynamicEntityReferenceFieldDefaultValueTest extends BrowserTestBase {
       'default_value_input[' . $field_name . '][0][target_id]' => $referenced_node_type->label() . ' (' . $referenced_node_type->id() . ')',
       'default_value_input[' . $field_name . '][1][target_id]' => $referenced_node_type2->label() . ' (' . $referenced_node_type2->id() . ')',
     ];
-    $this->drupalPostForm('admin/structure/types/manage/reference_content/fields/node.reference_content.' . $field_name, $field_edit, t('Save settings'));
+    $this->drupalGet('admin/structure/types/manage/reference_content/fields/node.reference_content.' . $field_name);
+    $this->submitForm($field_edit, t('Save settings'));
 
     // Check that the field has a dependency on the default value.
     $config_entity = $this->config('field.field.node.reference_content.' . $field_name)->get();
