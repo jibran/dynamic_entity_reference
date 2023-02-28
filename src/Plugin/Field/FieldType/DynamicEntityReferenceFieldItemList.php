bed1f81efb50f53d41a96eae830b7d541310de83
dpi <9354-dpi@users.noreply.drupalcode.org>
2023-02-28 23:11:49 +0000

    Issue #3344614 by dpi: Generic typehints for DynamicEntityReferenceFieldItemList

--- src/Plugin/Field/FieldType/DynamicEntityReferenceFieldItemList.php
+++ src/Plugin/Field/FieldType/DynamicEntityReferenceFieldItemList.php
@@ -11,7 +11,10 @@ use Drupal\Core\Form\FormStateInterface;
 /**
  * Defines a item list class for dynamic entity reference fields.
  *
- * @property DynamicEntityReferenceItem[] list
+ * @property \Drupal\dynamic_entity_reference\Plugin\Field\FieldType\DynamicEntityReferenceItem[] $list
+ * @method T|null offsetGet($offset)
+ * @template T of \Drupal\dynamic_entity_reference\Plugin\Field\FieldType\DynamicEntityReferenceItem
+ * @extends \Drupal\Core\Field\EntityReferenceFieldItemList<T>
  */
 class DynamicEntityReferenceFieldItemList extends EntityReferenceFieldItemList {
 
